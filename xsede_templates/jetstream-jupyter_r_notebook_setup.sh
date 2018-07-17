0760a61dd18bb08f973fc6da888a3d171619e6e3
Alexandra Paxton <a-paxton@users.noreply.github.com>
2018-07-16 21:45:16 -0400

    Update jetstream-jupyter_r_notebook_setup.sh

--- xsede_templates/jetstream-jupyter_r_notebook_setup.sh
+++ xsede_templates/jetstream-jupyter_r_notebook_setup.sh
@@ -4,10 +4,10 @@
 docker pull jupyter/r-notebook
 
 # start container with the r-notebook Docker image
-docker run --name pac_crqa -p 80:8888 -v $(pwd):/home/jovyan/work -e GRANT_SUDO=yes -d jupyter/r-notebook jupyter notebook --ip=0.0.0.0
+docker run --name my_container_name -p 80:8888 -v $(pwd):/home/jovyan/work -e GRANT_SUDO=yes -d jupyter/r-notebook jupyter notebook --ip=0.0.0.0
 
 # wait briefly
 sleep 3
 
 # show us where the Jupyter notebook server is running
-docker exec pac_crqa jupyter notebook list
+docker exec my_container_name jupyter notebook list
